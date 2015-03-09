c47f3e8b21b4fc15cfc3ef84ab9ea4f423ada444
Joe Ross <git@rossesmail.com>
2015-03-08 17:45:04 -0700

    Fix NPE

--- src/org/usfirst/frc330/wpilibj/BBDoubleSolenoid.java
+++ src/org/usfirst/frc330/wpilibj/BBDoubleSolenoid.java
@@ -27,7 +27,13 @@ public class BBDoubleSolenoid extends DoubleSolenoid {
 	}
 	
 	public int getInt() {
-		return value.value;
+		if (value == Value.kOff)
+			return Value.kOff_val;
+		else if (value == Value.kForward)
+			return Value.kForward_val;
+		else if (value == Value.kReverse)
+			return Value.kReverse_val;
+		return -1;
 	}
 
 }
