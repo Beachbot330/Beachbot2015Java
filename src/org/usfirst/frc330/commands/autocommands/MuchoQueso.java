da761c50a0e53833216da81c8e16ffc2be53137c
Joe Ross <git@rossesmail.com>
2015-04-18 18:26:55 -0700

    Closer to working, needs last can work

--- src/org/usfirst/frc330/commands/autocommands/MuchoQueso.java
+++ src/org/usfirst/frc330/commands/autocommands/MuchoQueso.java
@@ -10,6 +10,7 @@
 
 
 package org.usfirst.frc330.commands.autocommands;
+import org.usfirst.frc330.Robot;
 import org.usfirst.frc330.commands.ArmPID_on;
 import org.usfirst.frc330.commands.BuzzerBeepTimed;
 import org.usfirst.frc330.commands.CenterGrabberClose;
@@ -64,67 +65,73 @@ public class MuchoQueso extends BBCommandGroup {
         // a CommandGroup containing them would require both the chassis and the
         // arm.
     	
+    	PIDGains HardScrub = new PIDGains(0.3, 0, 0, 0, 1.0, 0.5, "HardScrub"); //p,  i,  d,  f,  maxOutput, maxOutputStep, name
+    	
     	//Hold joint angles, open grabbers, close center grabber
     	addParallel(new ArmPID_on());
     	addParallel(new WristPID_on());
-    	//addParallel(new LiftPID_on());
     	addSequential(new ShiftLow());
     	addSequential(new OpenAllGrabbers());
-    	//addParallel(new MastPID_on());
-    	addSequential(new SetMastPosition(120.0, 1.0, 0.3));
-    	//addSequential(new DriveDistance(2,0.5,0.5, true));
     	addParallel(new CenterGrabberClose());
     	addSequential(new Wait(0.1));
     	
-    	//Adjust the mast to vertical and start raising the arm
+    	//Start raising the arm
     	addParallel(new SetWristAngle(5.0));
-    	addSequential(new SetArmPosition(-12.9, 1.0, 0.1));  //Plan on it timing out, and the feedforward finishing this
-    	addParallel(new SetMastPosition(MastPos.vertical, 3.0));
-    	addSequential(new Wait(0.1));
     	addParallel(new SetArmPosition(33.0, 2.0));
-    	addSequential(new Wait(0.3));
-    	//addParallel(new SetWristAngle(5.0));
+    	addSequential(new Wait(0.1));
     	
     	//Drive to second station while loading first tote
-    	BBCommand driveCommand = new DriveWaypoint(0.0, 73.0, 4.0, 3.0, true);  //X Y Tol Timeout Stop
+    	BBCommand driveCommand = new DriveWaypoint(0.0, 92.0, 1.5, 3.0, false);  //X Y Tol Timeout Stop
     	addParallel(driveCommand);
     	addSequential(new SetLiftPosition(LiftPos.dropOff));
+    	addSequential(new Wait(0.5));
     	addSequential(new SetLiftPosition(LiftPos.justOverOneTote));
     	addSequential(new Wait(0.25));
+//    	addSequential(new Wait(3.30));
     	
     	//Pickup second tote
     	addSequential(new SetLiftPosition(LiftPos.dropOff), 0.5);
     	addSequential(new CheckDone(driveCommand));
-    	addParallel(new DriveDistanceAtRelAngle_NoTurn(24, 0.0, 1.5));  //Dist Angle Tol
-    	addSequential(new Wait(0.7));
+    	driveCommand = new DriveDistanceAtAbsAngle_NoTurn(24, 0.0, 1.5, 1.5, false);
+    	addParallel(driveCommand);  //Dist Angle Tol
+    	addSequential(new Wait(0.5));
+    	addSequential(new SetLiftPosition(5.0));
+    	addSequential(new CheckDone(driveCommand));
+    	addSequential(new DriveDistanceAtAbsAngle_NoTurn(-4.0, 0.0, 1.5, 0.5, false));
     	addParallel(new SetLiftPosition(17.3));
     	addSequential(new Wait(0.1));  //reduced from 0.2
+//    	addSequential(new Wait(3.30));
     
     	//Drive back from second tote
     	BBCommand moveArm = new SetArmPosition(-15, 4.0);
-    	driveCommand = new DriveDistanceAtRelAngle_NoTurn(-24 , 0.0, 1.5);  //double distance, double angle, double tolerance, double timeout
+    	driveCommand = new DriveDistanceAtAbsAngle_NoTurn(-28 , 0.0, 1.5, 1.5, false);  //double distance, double angle, double tolerance, double timeout
     	addParallel(driveCommand);  //Dist Angl Tol
     	addSequential(new Wait(1.3));
     	addParallel(moveArm);
     	addSequential(new CheckDone(driveCommand));
-
+//    	addSequential(new Wait(3.30));
     	
-    	//Drive Forward and Grab Can1 and Can2 at angle
+    	//Drop First Can
     	addSequential(new CheckDone(moveArm));
     	addSequential(new CenterGrabberOpen());
     	addParallel(new SetWristAngle(0.0, 3.0, 0.5));  //angle tolerance timeout
     	addSequential(new SetArmPosition(-35, 2.0));
+//    	addSequential(new Wait(3.30));
+    	
+    	//Drive Backwards to clear dropped can
+    	addSequential( new DriveDistanceAtAbsAngle_NoTurn(-12 , 0.0, 1.5, 1.0, false));
+//    	addSequential(new Wait(3.30));
     	
     	//Drive to last location
-    	PIDGains GyroLow = new PIDGains(0.03, 0.00, 0.0, 0.0, 1.0, 1.0, "GyroLow"); //p,  i,  d,  f,  maxOutput, maxOutputStep
+    	PIDGains GyroLow = new PIDGains(0.03, 0.00, 0.02, 0.0, 1.0, 1.0, "GyroLow"); //p,  i,  d,  f,  maxOutput, maxOutputStep
 //    	addSequential(new TurnGyroWaypoint(0.0, -155.0, 4.0, 3.0, GyroLow, ChassisConst.GyroDriveHigh));
-    	addSequential(new TurnGyroAbs(179.0,4.0, 3.0, false, true, GyroLow, ChassisConst.GyroTurnHigh));
+    	addSequential(new TurnGyroAbs(179.0,5.0, 3.0, false, true, GyroLow, ChassisConst.GyroTurnHigh));
+    	addParallel(new SetArmPosition(-12.0, 1.0));
     	addSequential(new Wait(3.30));
-    	driveCommand = new DriveWaypoint(0.0, -155.0, 4.0, 3.0, true, ChassisConst.DriveLow, ChassisConst.DriveHigh, GyroLow, ChassisConst.GyroDriveHigh);
+    	driveCommand = new DriveWaypoint(0.0, 40.0, 4.0, 3.0, false, ChassisConst.DriveLow, ChassisConst.DriveHigh, GyroLow, ChassisConst.GyroDriveHigh);
     	addParallel(driveCommand);
     	addParallel(new SetLiftPosition(LiftPos.justOverOneTote));
-    	addSequential(new Wait(0.6));
-    	addParallel(new SetArmPosition(-8.0, 1.0));
+    	addSequential(new Wait(0.2));
     	addSequential(new CheckDone(driveCommand));
     	addSequential(new Wait(3.30));
     	
@@ -135,7 +142,7 @@ public class MuchoQueso extends BBCommandGroup {
     	addSequential(new Wait(3.30));
     	
     	//Drive to last tote
-    	addSequential(new DriveWaypoint(0.0, 174.0, 4.0, 3.0, true));
+    	addSequential(new DriveWaypoint(0.0, -10.0, 4.0, 3.0, true));
     	addSequential(new Wait(3.30));
     	
     	//Load third tote
@@ -151,12 +158,16 @@ public class MuchoQueso extends BBCommandGroup {
     	//Turn 90
     	addSequential(new TurnAngleAggressive(90.0, 2.0));  //angle, timeout
     	addSequential(new ShiftHigh());
-    	addSequential(new DriveDistanceAtAbsAngle_NoTurn(70.0, 90.0, 4.0));  //Dist Angl Tol
+    	addSequential(new DriveDistanceAtAbsAngle_NoTurn(90.0, 90.0, 4.0));  //Dist Angl Tol
     	addSequential(new SetLiftPosition(LiftPos.dropOff));
     	addSequential(new ShiftLow());
-    	addSequential(new DriveDistanceAtAbsAngle_NoTurn(-10.0, 90.0, 4.0));  //Dist Angl Tol
+    	addSequential(new DriveDistanceAtAbsAngle_NoTurn(-20.0, 90.0, 4.0));  //Dist Angl Tol
     	addSequential(new BuzzerBeepTimed());
     	addSequential(new BuzzerBeepTimed());
     	addSequential(new BuzzerBeepTimed());
+    }
+    
+    protected void end() {
+    	Robot.logger.println("MuchoQueso end time: " + this.timeSinceInitialized());
     }
 }
