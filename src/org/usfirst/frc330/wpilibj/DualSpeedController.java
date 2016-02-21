caabab7d24c893f2819736e6a0686edfc88a0a41
Joe Ross <git@rossesmail.com>
2016-02-20 16:16:29 -0800

    Fix compile error for midseason update

--- src/org/usfirst/frc330/wpilibj/DualSpeedController.java
+++ src/org/usfirst/frc330/wpilibj/DualSpeedController.java
@@ -113,4 +113,10 @@ public class DualSpeedController implements SpeedController, LiveWindowSendable 
 		return motor1.getInverted();
 	}
 
+	@Override
+	public void stopMotor() {
+		motor1.stopMotor();
+		motor2.stopMotor();
+	}
+
 }
